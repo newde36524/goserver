4f374639d48aebd77161385e276e1ecfe0d4f0c7
zsk <718114245@qq.com>
2019-07-29 21:28:27 +0800

    you know ? i have a dream !

--- example/server/customer/log_handle.go
+++ example/server/customer/log_handle.go
@@ -30,10 +30,12 @@ func (LogHandle) OnConnection(conn *srv.Conn, next func()) {
 func (LogHandle) OnMessage(conn *srv.Conn, p srv.Packet, next func()) {
 	logs.Infof("日志模块输出:  开始计时")
 	startTime := time.Now()
+	defer func() {
+		endTime := time.Now()
+		sub := endTime.Sub(startTime).Seconds() * 1000
+		logs.Infof("日志模块输出:  开始时间:%s  结束时间:%s 总耗时: %f ms", startTime.Format("2006-01-02 15:04:05"), endTime.Format("2006-01-02 15:04:05"), sub)
+	}()
 	next()
-	endTime := time.Now()
-	sub := endTime.Sub(startTime).Seconds() * 1000
-	logs.Infof("日志模块输出:  开始时间:%s  结束时间:%s 总耗时: %f ms", startTime.Format("2006-01-02 15:04:05"), endTime.Format("2006-01-02 15:04:05"), sub)
 }
 
 //OnClose .
