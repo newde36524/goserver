f61bbdbc2c6275de5cc62e57c09d8be8baa76d19
root <root@DESKTOP-NVORJB6.localdomain>
2020-02-27 22:36:15 +0800

    修改http协议服务器

--- example/httpserver/handles/handle.go
+++ example/httpserver/handles/handle.go
@@ -2,13 +2,14 @@ package handles
 
 import (
 	"bufio"
-	"bytes"
 	"context"
 	"encoding/json"
 	"io"
 	"mime/multipart"
 	"net/http"
 	"net/url"
+	"strconv"
+	"time"
 
 	"github.com/newde36524/goserver"
 
@@ -49,20 +50,43 @@ func (RootHandle) ReadPacket(ctx context.Context, conn goserver.Conn, next func(
 		} else {
 			logs.Error(err)
 		}
-		res, err := http.ReadResponse(bytes.NewReader([]byte("嘤嘤嘤")), req)
-		if err != nil {
-			logs.Error(err)
-		}
-		// res.
-		logs.Infof("%#v", req)
+		// res, err := http.ReadResponse(bytes.NewReader([]byte("嘤嘤嘤")), req)
+		// if err != nil {
+		// 	logs.Error(err)
+		// }
+		// // res.
+		// logs.Infof("%#v", req)
 	} else {
 		logs.Error(err)
 	}
+	resp := appendResp(nil, "200 OK", "", "o huo~~~~~")
 	p := &goserver.P{}
-	p.SetBuffer()
+	p.SetBuffer(resp)
 	conn.Write(p)
 	conn.Close()
 	return nil
+}
+
+func appendResp(b []byte, status, head, body string) []byte {
+	b = append(b, "HTTP/1.1"...)
+	b = append(b, ' ')
+	b = append(b, status...)
+	b = append(b, '\r', '\n')
+	b = append(b, "Server: HttpServer\r\n"...)
+	b = append(b, "Date: "...)
+	b = time.Now().AppendFormat(b, "Mon, 02 Jan 2006 15:04:05 GMT")
+	b = append(b, '\r', '\n')
+	if len(body) > 0 {
+		b = append(b, "Content-Length: "...)
+		b = strconv.AppendInt(b, int64(len(body)), 10)
+		b = append(b, '\r', '\n')
+	}
+	b = append(b, head...)
+	b = append(b, '\r', '\n')
+	if len(body) > 0 {
+		b = append(b, body...)
+	}
+	return b
 }
 
 type Request struct {
--- example/httpserver/main.go
+++ example/httpserver/main.go
@@ -6,7 +6,7 @@ import (
 	"runtime"
 	"time"
 
-	srv "github.com/newde36524/goserver"
+	"github.com/newde36524/goserver"
 	"github.com/newde36524/goserver/example/httpserver/handles"
 
 	"github.com/issue9/logs"
@@ -28,21 +28,59 @@ func init() {
 }
 
 func main() {
+	// address := "0.0.0.0:12336"
+	// logger, err := srv.NewDefaultLogger()
+	// opt := srv.ConnOption{
+	// 	SendTimeOut: 1 * time.Minute, //发送消息包超时时间
+	// 	RecvTimeOut: 1 * time.Minute, //接收消息包超时时间
+	// 	Logger:      logger,          //日志打印对象
+	// }
+	// server, err := srv.TCPServer(opt)
+	// if err != nil {
+	// 	logs.Error(err)
+	// }
+	// server.Use(handles.RootHandle{})
+	// server.UseDebug()
+	// server.Binding(address)
+	// logs.Infof("服务器开始监听...  监听地址:%s", address)
+	// fmt.Scanln()
+	// <-context.Background().Done()
+
 	address := "0.0.0.0:12336"
-	logger, err := srv.NewDefaultLogger()
-	opt := srv.ConnOption{
-		SendTimeOut: 1 * time.Minute, //发送消息包超时时间
-		RecvTimeOut: 1 * time.Minute, //接收消息包超时时间
-		Logger:      logger,          //日志打印对象
-	}
-	server, err := srv.TCPServer(opt)
+	server, err := goserver.TCPServer(goserver.ModOption(func(opt *goserver.ConnOption) {
+		logger, err := goserver.NewDefaultLogger()
+		if err != nil {
+			fmt.Println(err)
+		}
+		opt.SendTimeOut = time.Minute //发送消息包超时时间
+		opt.RecvTimeOut = time.Minute //接收消息包超时时间
+		opt.HandTimeOut = time.Minute //处理消息包超时时间
+		opt.Logger = logger           //日志打印对象
+		// opt.ParallelSize = 10
+		// opt.MaxGopollExpire = 3 * time.Second
+	}))
 	if err != nil {
 		logs.Error(err)
 	}
-	server.Use(handles.RootHandle{})
+	server.UsePipe().
+		// Regist(new(customer.LogHandle)).
+		Regist(new(handles.RootHandle))
+	// Regist(handle.NewTraceHandle())
 	server.UseDebug()
 	server.Binding(address)
 	logs.Infof("服务器开始监听...  监听地址:%s", address)
 	fmt.Scanln()
+	// signalCh := make(chan os.Signal)
+	// signal.Notify(signalCh, os.Interrupt)
+	// go func() {
+	// 	for {
+	// 		select {
+	// 		case sign := <-signalCh:
+	// 			fmt.Println("接收到消息:", sign)
+	// 			sign.Signal()
+	// 		}
+	// 	}
+	// }()
+
 	<-context.Background().Done()
 }
